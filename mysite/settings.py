1ba6bc1d1ce5bbf24dd5e737623c084e10cfc0f5
Licho59 <lestlalka@gmail.com>
2021-04-09 21:03:25 +0200

    safety improvement

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from my_key import my_blog_key
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w^-f7#t5a78=jgx)@vu54n56oo(7re^sk#=y!d4pvodmc)34^j'
+SECRET_KEY = my_blog_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
