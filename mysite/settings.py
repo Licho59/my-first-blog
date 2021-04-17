f449f64093082be94af6b065ebe1941b0ba539d4
Licho59 <lestlalka@gmail.com>
2021-04-17 22:30:12 +0200

    once more settings correct

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-from my_key import my_blog_key
+#from my_key import my_blog_key
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
